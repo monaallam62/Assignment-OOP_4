21f5a5d9858f44224a69ba126eab520d9ab3724b
monaallam62 <monaallam5102@gmail.com>
2026-03-08 16:58:13 +0200

    SeatLocation Struct

---
+++ Assignment OOP_4/Methods/SeatLocation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment_OOP_4.Enum;
+
+namespace Assignment_OOP_4.Methods
+{
+    #region SeatLocation Struct
+    public struct SeatLocation
+    {
+        public TypeOfTicket Row { get; set; }
+        public int Number { get; set; }
+
+        public SeatLocation(TypeOfTicket row, int number)
+        {
+            Row = row;
+            Number = number;
+        }
+        public override string ToString()
+        {
+            return $"{Row}-{Number}";
+            ;
+        }
+    }
+    #endregion
+}
